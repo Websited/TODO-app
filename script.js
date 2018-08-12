9adfd623e85e139c760827e9d1f736433cd56382
ppierzchalka <przemekpierzchalka@gmail.com>
2018-08-13 01:08:34 +0200

    add localStorageWrite function + beautify

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const today = new Date();
 
 const todoItem = class {
-  constructor(id,done = false, name, deadline) {
+  constructor(id, done = false, name, deadline) {
     this.id = id,
-    this.done = done;
+      this.done = done;
     this.name = name;
     this.deadline = deadline;
   }
   convertDeadline() {
-    const year = this.deadline.toString().substring(0,4)
-    const month = this.deadline.toString().substring(4,6)-1
-    const day = this.deadline.toString().substring(6,8)
-    return new Date(year,month,day);
+    const year = this.deadline.toString().substring(0, 4)
+    const month = this.deadline.toString().substring(4, 6) - 1
+    const day = this.deadline.toString().substring(6, 8)
+    return new Date(year, month, day);
   }
 
   edit(name) {
@@ -36,14 +36,13 @@ class todoItemCollection extends Array {
   }
   remove(id) {
     const toRemove = this.indexOf(this.filter(item => item.id === id)[0]);
-    this.splice(toRemove,1);
+    this.splice(toRemove, 1);
   }
 }
 
-const todos = new todoItemCollection(
-);
+const todos = new todoItemCollection();
 
-function generateTodoMarkup(todo){
+function generateTodoMarkup(todo) {
   return `<div class="panel panel-default todo-item ${todo.done ? "finished" : ""} ${todo.deadline === "" ? "" : (todo.convertDeadline() < today ? "past-due" : "")}" >
             <div class="panel-body">
                 <div class="row">
@@ -84,30 +83,27 @@ function createTodoList(todos) {
 
 function deleteTodo(id) {
   todos.remove(id);
-  localStorage.setItem('todos', JSON.stringify(todos));
-  createTodoList(todos);
+  localStorageWrite();
 }
 
 function toggleDone(id) {
   todos.filter(item => item.id === id)[0].toggleCheckbox();
-  localStorage.setItem('todos', JSON.stringify(todos));
-  createTodoList(todos);
+  localStorageWrite();
 }
 
 
 function handleData() {
   const name = document.getElementById("todo-name");
   const date = document.getElementById("todo-date");
-  const id = todos.length > 0 ? todos[todos.length-1].id + 1 : 0;
+  const id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 0;
   todos.add(new todoItem(id, false, name.value, date.value.split("-").join("")));
-  createTodoList(todos);
   name.value = ""
   date.value = ""
-  localStorage.setItem('todos', JSON.stringify(todos));
+  localStorageWrite();
 }
 
 function appendForm(todoNameElement, todoId) {
-  todoNameElement.innerHTML = `<form onsubmit="event.preventDefault(); editName(${todoId});"><input id='new-name' type="text"/></form>`
+  todoNameElement.innerHTML = `<form onsubmit="event.preventDefault(); editName(${todoId});"><span class="input-group-text">Press Enter to save</span><input id='new-name' type="text"/></form>`
 }
 
 function editName(todoId) {
@@ -116,8 +112,14 @@ function editName(todoId) {
   localStorage.setItem('todos', JSON.stringify(todos));
   createTodoList(todos);
 }
+
+function localStorageWrite() {
+  localStorage.setItem('todos', JSON.stringify(todos));
+  createTodoList(todos);
+}
+
 var retrievedTodos = JSON.parse(localStorage.getItem('todos'));
-retrievedTodos.forEach(function(todo){
-  todos.add(new todoItem(todo.id,todo.done,todo.name,todo.deadline))
+retrievedTodos.forEach(function(todo) {
+  todos.add(new todoItem(todo.id, todo.done, todo.name, todo.deadline))
   createTodoList(todos);
 });
