774b0ae066294b9e18dcfc922a40076471828614
ppierzchalka <przemekpierzchalka@gmail.com>
2018-08-13 22:31:51 +0200

    fixed date day-1 issue

--- script.js
+++ script.js
@@ -3,13 +3,7 @@ const TodoItem = class {
     this.id = id,
       this.done = done;
     this.name = name;
-    this.deadline = deadline;
-  }
-  convertDeadline() {
-    const year = this.deadline.toString().substring(0, 4);
-    const month = this.deadline.toString().substring(4, 6) - 1;
-    const day = this.deadline.toString().substring(6, 8);
-    return new Date(year, month, day);
+    this.deadline = deadline === "" ? "" : new Date(Date.UTC(deadline.toString().substring(0, 4), deadline.toString().substring(4, 6) - 1, deadline.toString().substring(6)));
   }
 
   edit(name) {
@@ -43,40 +37,43 @@ const HTMLTodoIRenderer = {
     const today = new Date();
     const listItem = document.getElementById("list");
     const todoList = [];
-    todos.forEach(function(todoObj){
-      function generateTodoMarkup(todoObj) {
-        return `<div class="panel panel-default todo-item ${todoObj.done ? "finished" : ""} ${todoObj.deadline === "" ? "" : (todoObj.convertDeadline() < today ? "past-due" : "")}" >
-                          <div class="panel-body">
-                              <div class="row">
-                                  <div class="col-sm-2">
-                                      <div class="checkbox">
-                                          <label>
-                                              <input onclick=TodoApp.toggleDone(${todoObj.id}) type="checkbox" ${todoObj.done ? "checked" : ""}>
-                                          </label>
-                                      </div>
-                                  </div>
-                                  <div class="col-sm-6">
-                                      <h4 class="todo-name" id="${todoObj.id}-name" ondblclick="TodoApp.appendForm(this,'${todoObj.name}', ${todoObj.id})">
-                                          ${todoObj.name}
-                                      </h4>
-                                  </div>
-                                  <div class="col-sm-2">
-                                      <h4 class="todo-date">
-                                          ${todoObj.deadline ?  todoObj.convertDeadline().toISOString().slice(0, 10) : "No deadline"}
-                                      </h4>
-                                  </div>
-                                  <div class="col-sm-2">
-                                      <button onclick="TodoApp.deleteTodo(${todoObj.id})" type="submit" class="btn btn-block btn-danger" name="remove todo">Remove</button>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>`
-        };
-      const todo = generateTodoMarkup(todoObj);
-      todoList.push(todo);
-      const listItem = document.getElementById("list");
-      listItem.innerHTML = todoList.join("");
-    });
+    if (todos.length > 0) {
+      todos.forEach(function(todoObj){
+        function generateTodoMarkup(todoObj) {
+          return `<div class="panel panel-default todo-item ${todoObj.done ? "finished" : ""} ${todoObj.deadline === "" ? "" : (todoObj.deadline < today ? "past-due" : "")}" >
+                            <div class="panel-body">
+                                <div class="row">
+                                    <div class="col-sm-2">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input onclick=TodoApp.toggleDone(${todoObj.id}) type="checkbox" ${todoObj.done ? "checked" : ""}>
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <h4 class="todo-name" id="${todoObj.id}-name" ondblclick="TodoApp.appendForm(this,'${todoObj.name}', ${todoObj.id})">
+                                            ${todoObj.name}
+                                        </h4>
+                                    </div>
+                                    <div class="col-sm-2">
+                                        <h4 class="todo-date">
+                                            ${todoObj.deadline ?  todoObj.deadline.toISOString().substring(0,10) : "No deadline"}
+                                        </h4>
+                                    </div>
+                                    <div class="col-sm-2">
+                                        <button onclick="TodoApp.deleteTodo(${todoObj.id})" type="submit" class="btn btn-block btn-danger" name="remove todo">Remove</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`
+          };
+        const todo = generateTodoMarkup(todoObj);
+        todoList.push(todo);
+        listItem.innerHTML = todoList.join("");
+      });
+    } else {
+      listItem.innerHTML = "";
+    }
   }
 }
 
@@ -111,7 +108,7 @@ const TodoApp = {
     var retrievedTodos = JSON.parse(localStorage.getItem('todos'));
     if (retrievedTodos) {
       retrievedTodos.forEach(function(todo) {
-        todos.add(new TodoItem(todo.id, todo.done, todo.name, todo.deadline))
+        todos.add(new TodoItem(todo.id, todo.done, todo.name,todo.deadline.substring(0,10).split("-").join("")))
         HTMLTodoIRenderer.render(todos);
       });
     } else {
