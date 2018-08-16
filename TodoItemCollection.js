bcef959e60dd0b525792b2256502cd91d90d91e8
Michal Przybys <michal.przybys@gmail.com>
2018-08-16 15:50:20 +0200

    Added comment

--- TodoItemCollection.js
+++ TodoItemCollection.js
@@ -1,3 +1,8 @@
+/**
+ * extending an Array might not be the best idea
+ * it's not supported by older browsers and can't be polyfilled properly
+ * but what's more important it doesn't provide any additional features in this case
+ */
 class TodoItemCollection extends Array {
   constructor(...todos) {
     super(...todos);
