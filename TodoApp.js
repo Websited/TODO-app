bedef2dd40ae0b62bb7e1fc3396ef8c730e681bf
ppierzchalka <przemekpierzchalka@gmail.com>
2018-11-30 01:55:07 +0100

    variable name change

--- TodoApp.js
+++ TodoApp.js
@@ -1,48 +1,48 @@
 const TodoApp = {
    handleData: function() {
-    const name = document.getElementById("todo-name");
+    const title = document.getElementById("todo-title");
     const date = document.getElementById("todo-date");
     const id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 0;
-    todos.add(new TodoItem(id, false, name.value, date.value.split("-").join("")));
-    name.value = ""
+    todos.add(new TodoItem(id, false, title.value, date.value.split("-").join("")));
+    title.value = ""
     date.value = ""
-    this.dataWrite('myTodos', todos);
+    this.dataWrite('todos', todos);
   },
-  toggleDone: function(id) {
+  toggleCompleted: function(id) {
     todos.filter(item => item.id === id)[0].toggleCheckbox();
-    this.dataWrite('myTodos', todos);
+    this.dataWrite('todos', todos);
   },
    deleteTodo: function(id) {
     todos.remove(id);
-    this.dataWrite('myTodos', todos);
+    this.dataWrite('todos', todos);
   },
   deleteCompleted: function() {
-    completedTodos = todos.filter(todo => todo.done === true);
+    completedTodos = todos.filter(todo => todo.completed === true);
     completedTodos.forEach(todo => TodoApp.deleteTodo(todo.id));
     console.log(todos);
   },
-  appendForm: function(todoNameElement,name, todoId) {
-    todoNameElement.innerHTML = `<form class="name-edit" onsubmit="event.preventDefault(); TodoApp.editName(${todoId});"><input maxlength="20" id='new-name' type="text" value=${name} required/></form>`
+  appendForm: function(todoTitleElement,title, todoId) {
+    todoTitleElement.innerHTML = `<form class="title-edit" onsubmit="event.preventDefault(); TodoApp.edittitle(${todoId});"><input maxlength="20" id='new-title' type="text" value=${title} required/></form>`
   },
-  editName: function(todoId) {
+  edittitle: function(todoId) {
     const todo = todos.filter(todo => todo.id === todoId)[0];
-    todo.edit(document.getElementById('new-name').value);
+    todo.edit(document.getElementById('new-title').value);
     localStorage.setItem('todos', JSON.stringify(todos));
     HTMLTodoIRenderer.render(todos);
   },
-  dataRead: function(todoCollectionName, api = 'localStorage') {
-    var retrievedTodos = JSON.parse(apis[api].read(todoCollectionName));
+  dataRead: function(todoCollectiontitle, api = 'localStorage') {
+    var retrievedTodos = JSON.parse(apis[api].read(todoCollectiontitle));
     if (retrievedTodos) {
       retrievedTodos.forEach(function(todo) {
-        todos.add(new TodoItem(todo.id, todo.done, todo.name,todo.deadline.substring(0,10).split("-").join("")))
+        todos.add(new TodoItem(todo.id, todo.completed, todo.title,todo.deadline.substring(0,10).split("-").join("")))
         HTMLTodoIRenderer.render(todos);
       });
     } else {
       HTMLTodoIRenderer.render(todos);
     }
   },
-  dataWrite: function(todoCollectionName, todoData, api = 'localStorage') {
-    apis[api].write(todoCollectionName, todoData);
+  dataWrite: function(todoCollectiontitle, todoData, api = 'localStorage') {
+    apis[api].write(todoCollectiontitle, todoData);
     HTMLTodoIRenderer.render(todoData);
   }
 };
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const todos = new TodoItemCollection();
-TodoApp.dataRead('myTodos');
+TodoApp.dataRead('todos');
 
 runSuites();
